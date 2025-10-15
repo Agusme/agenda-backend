4fa6fcdc6012868718d0378cb1629a1db4d041b9
agusmena <agusmena60@mail.com>
2025-10-15 12:49:55 -0300

    feat: add mongoose and property model

---
+++ src/models/property.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const propertySchema = new Schema({
+  typeProperty: {
+    require: true,
+    type: String,
+    enum: ["Casa", "Departamento", "Terreno", "Local"],
+  },
+  TypeTransaction: {
+    require: true,
+    type: String,
+    enum: ["Venta", "Alquiler"],
+  },
+  bathroom: {
+    type: Number,
+  },
+  bedroom: {
+    type: Number,
+  },
+  location: {
+    require: true,
+    type: String,
+    maxlenght: 100,
+  },
+  description: {
+    require: true,
+    type: String,
+  },
+  map: {
+    type: String,
+  },
+  images: {
+    type: [String],
+  },
+});
+
+const Property = mongoose.model("Property", propertySchema);
+
+export default Property;
--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -6,4 +6,27 @@ router.get("/", (req, res)=>{
     res.json({message: 'Obteniendo todas las propiedades'})
 })
 
+router.post('/',(req, res)=>{
+    res.json({message:'Creando una nueva propiedad', data:req.body})
+})
+
+router.get('/:id', (req,res)=> {
+    const{id}=req.params
+    res.json({message:`Obteniendo la prop con id : ${id}`})
+})
+router.put('/id', (req,res)=>{
+    const {id}=req.params
+    res.json({message:`Editando prop con id:${id}`})
+})
+router.delete('/id', (req, res)=>{
+    const {id}= req.params
+    res.json({message:`Eliminando la prop con id : ${id}`})
+})
+
+router.get('/search',(req,res)=>{
+    const {q}=req.query
+    res.json({message:`Buscando prop con el termino: ${q}`})
+})
+
+
 export default router
