94350dda463675395df7e29dad119d1ecc9112f1
agusmena <agusmena60@mail.com>
2025-10-16 10:06:56 -0300

    feat: add controller property

--- src/controllers/property.controllers.js
+++ src/controllers/property.controllers.js
@@ -8,3 +8,42 @@ try {
     res.status(500).json({message:'Error al obtener propiedades', error})
 }
 }
+
+export const create = async(req, res)=>{
+    try {
+        const newProperty = await propertyService.createProperty(req.body);
+        res.status(201).json(newProperty)
+    } catch (error) {
+        res.status(400).json({message:'Error al crear propiedad', error})
+    }
+}
+
+export const getOne = async(req, res)=>{
+    try {
+        const property = await propertyService.getPropertyById(req.params.id);
+        if(!property)
+            return res.status(404).json({message:'Propiedad no encontrada'});
+        res.status(200).json({message:property})
+    } catch (error) {
+        res.status(500).json({message:'Error al obtener propiedad', error})
+    }
+};
+
+export const update = async(req, res)=>{
+    try {
+        const updated= await propertyService.updateProperty(req.para.id, req.body)
+        res.status(200).json(updated)
+    } catch (error) {
+        res.status(400).json({message:'Error al actualizar propiedad', error})
+    }
+};
+
+export const remove =async(req, res)=>{
+    try {
+        await propertyService.deleteProperty(req.params.id);
+        res.status(200).json({message:'Propiedad eliminada'})
+    } catch (error) {
+        res.status(500).json({message:'Error al eliminar propiedad', error})
+    }
+};
+
--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -1,32 +1,22 @@
 import { Router } from "express";
-import { getAll } from "../controllers/property.controllers.js";
+import {
+  create,
+  getAll,
+  getOne,
+  remove,
+  update,
+} from "../controllers/property.controllers.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/",getAll
-)
+router.route('/')
+.get(getAll)
+.post(create)
 
-router.post('/',(req, res)=>{
-    res.json({message:'Creando una nueva propiedad', data:req.body})
-})
-
-router.get('/:id', (req,res)=> {
-    const{id}=req.params
-    res.json({message:`Obteniendo la prop con id : ${id}`})
-})
-router.put('/:id', (req,res)=>{
-    const {id}=req.params
-    res.json({message:`Editando prop con id:${id}`})
-})
-router.delete('/:id', (req, res)=>{
-    const {id}= req.params
-    res.json({message:`Eliminando la prop con id : ${id}`})
-})
-
-router.get('/search',(req,res)=>{
-    const {q}=req.query
-    res.json({message:`Buscando prop con el termino: ${q}`})
-})
+router.route('/:id')
+.get(getOne)
+.put(update)
+.delete(remove)
 
 
-export default router
+export default router;
